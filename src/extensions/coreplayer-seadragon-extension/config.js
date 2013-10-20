25ef3ad9c7d185c64ee665ef43041d030be07fc5
unknown <e.silverton@gmail.com>
2013-10-20 22:53:54 +0100

    removed session message

--- src/extensions/coreplayer-seadragon-extension/config.js
+++ src/extensions/coreplayer-seadragon-extension/config.js
@@ -19,7 +19,6 @@
                 "emptyValue": "please enter a value",
                 "pageNotFound": "page not found",
                 "ok": "OK",
-                "sessionExpired": "Your session has expired, please refresh to log in again",
                 "refresh": "Refresh",
                 "invalidNumber": "Please enter a valid number",
                 "pageNotFound": "This item does not contain a page with the number you entered. Try switching the numbering mode to 'image'"
