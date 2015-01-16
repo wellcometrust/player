70d037a377569fedcef930847d6b6b089f2ab961
Edward Silverton <e.silverton@gmail.com>
2015-01-16 14:10:14 +0000

    add 1 to anchorindex

--- src/modules/coreplayer-pdfcenterpanel-module/pdfCenterPanel.ts
+++ src/modules/coreplayer-pdfcenterpanel-module/pdfCenterPanel.ts
@@ -64,7 +64,7 @@ export class PDFCenterPanel extends baseCenter.CenterPanel {
 
                 PDFJS.DEFAULT_URL = canvas.mediaUri;
 
-                var anchorIndex = parseInt(this.extension.getParam(baseProvider.params.anchor)) || 0;
+                var anchorIndex = (1 + parseInt(this.extension.getParam(baseProvider.params.anchor))) || 0;
 
                 PDFView.initialBookmark = "page=" + anchorIndex;
 
